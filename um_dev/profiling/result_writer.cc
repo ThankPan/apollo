d46fc4bd53f21e78409e5f757c82aeae249396ec
lovelydett <xyt@bupt.cn>
2022-02-23 21:55:36 +0800

    [Yuting] profiling result writer basically works

--- um_dev/profiling/result_writer.cc
+++ um_dev/profiling/result_writer.cc
@@ -4,6 +4,8 @@
 
 #include "./result_writer.h"
 
+#include <assert.h>
+
 #include <iostream>
 
 namespace um_dev {
@@ -11,27 +13,52 @@ namespace profiling {
 
 ProfilingResultWriter* ProfilingResultWriter::instance_ =
     new ProfilingResultWriter();
+apollo::cyber::Time ProfilingResultWriter::last_write_time_ =
+    apollo::cyber::Time::Now();
 
-ProfilingResultWriter::ProfilingResultWriter() {
-  // TODO: (yuting) figure out the run-time working directory to write to
-  // fout_timing_.open("");
+ProfilingResultWriter::ProfilingResultWriter()
+    : timing_output_file_("/apollo/um_dev/profiling/results/timing_result_"),
+      memory_output_file_("/apollo/um_dev/profiling/results/memory_result_"),
+      gpu_output_file_("/apollo/um_dev/profiling/results/gpu_result_") {
+  apollo::cyber::Time now = apollo::cyber::Time::Now();
+  fout_timing_.open(timing_output_file_ + std::to_string(now.ToSecond()) +
+                    ".log");
+  assert(fout_timing_.is_open());
   std::cout << "[Profiling] Simulate open files\n";
+}
+
+ProfilingResultWriter::~ProfilingResultWriter() {
+  if (fout_timing_.is_open()) {
+    fout_timing_.close();
+  }
+  if (fout_memory_.is_open()) {
+    fout_memory_.close();
+  }
+  if (fout_gpu_.is_open()) {
+    fout_gpu_.close();
+  }
 }
 
 ProfilingResultWriter& ProfilingResultWriter::Instance() { return *instance_; }
 
 bool ProfilingResultWriter::write_to_file(PROFILING_METRICS profiling_type,
                                           const std::string& content) {
-  // TODO: (yuting) write to files here
+  // TODO: more reasonable throttle by timer rather than thread-unsafe static
+  // member
+  apollo::cyber::Time now = apollo::cyber::Time::Now();
   switch (profiling_type) {
     case TIMING: {
+      if (now - last_write_time_ < apollo::cyber::Duration(1.0)) {
+        return true;
+      }
       std::lock_guard<std::mutex> lock(mutex_timing_);
-      std::cout << "[Profiling] type = Timing, " << content << std::endl;
+      fout_timing_ << "[Profiling] type = Timing, " << content << std::endl;
       break;
     }
     default:
       break;
   }
+  last_write_time_ = now;
 
   return true;
 }
