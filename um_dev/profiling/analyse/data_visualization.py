9cb1421defe00bf87d222338a0a40375c7f5d514
lovelydett <xyt@bupt.cn>
2022-04-07 13:31:02 +0800

    [Yuting] analyse.py plot_comparative

--- um_dev/profiling/analyse/data_visualization.py
+++ um_dev/profiling/analyse/data_visualization.py
@@ -23,7 +23,7 @@ def load_data_from_file(file_path, results):
       line = line.strip()
       if len(line) == 0:
         continue
-      _, task, time = line.split()
+      _, _, task, time = line.split()
       task = task.strip(':')
       time = float(time.strip('ms'))
       if task in results:
@@ -59,6 +59,41 @@ def plot_data(results):
   plt.savefig("result.png")
   plt.show()
 
+def plot_comparative(result1, result2):
+  for task_name, data1 in result1.items():
+    if task_name not in result2:
+      continue
+    data2 = result2[task_name]
+    data1, data2 = np.array(data1), np.array(data2)
+    plt.title(task_name)
+
+    # Plot resul1
+    plt.plot(range(len(data1)), data1,
+             color='blue',
+             label='solo',
+             marker='*',
+             linewidth=0.8,
+             markersize=0.8
+             )
+
+    # Plot result2
+    plt.plot(range(len(data2)), data2,
+             color='red',
+             label='concurrent',
+             marker='^',
+             linewidth=0.8,
+             markersize=0.8
+             )
+    plt.ylabel("Latency(ms)")
+    plt.xlabel("No.")
+    plt.legend()
+    plt.axis([0, 60, 0, 200])
+    plt.savefig("result_" + task_name.replace('::', "_") + ".png")
+    plt.show()
+
+
 if __name__ == "__main__":
-  results = load_data_from_dir("/home/tt/Codes/apollo/um_dev/profiling/results/lgsvl_3_30")
-  plot_data(results)
+  result_solo = load_data_from_dir("/home/tt/Codes/apollo/um_dev/profiling/results/lgsvl_4_2_crash_case_solo_mode")
+  # plot_data(results)
+  result_whole = load_data_from_dir("/home/tt/Codes/apollo/um_dev/profiling/results/lgsvl_4_2_crash_case_whole_mode")
+  plot_comparative(result_solo, result_whole)
